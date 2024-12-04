4aec84833c198b1eb6543e3148dcb74e84d2f8c5
edsonrdl <91911044+edsonrdl@users.noreply.github.com>
2024-12-03 22:09:07 -0300

    rebase

---
+++ teste1.js
@@ -0,0 +1,4 @@
+console.log("teste 01 funcionou ");
+console.log("teste 02?");
+
+console.log("teste 03?");
---
+++ teste2.js
@@ -0,0 +1,6 @@
+console.log("teste-1 01 funcionou ");
+console.log("teste-2 02?");
+
+console.log("teste-3 03?");
+console.log("teste-4 03?");
+console.log("teste-5 03?");
